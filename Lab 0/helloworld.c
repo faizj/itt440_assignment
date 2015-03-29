601c7be8ed5c35c4a1b4fed9641f5d4b8363d17f
Faiz <mohdfaizjamaludin@yahoo.com>
2015-03-29 14:20:57 +0800

    Lab 0 exercise

---
+++ Lab 0/helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+/*this is my first program with C */
+
+printf("Hello World\n");
+printf("I Love C\n\n");
+
+return (0);
+}
---
+++ Lab 0/userinput.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+/*This is mu second program in C*/
+
+int age;
+
+printf("\nHi, How old are you? > ");
+scanf("%d", &age);
+
+printf("You are %d years old\n\n", age);
+
+return (0);
+}
